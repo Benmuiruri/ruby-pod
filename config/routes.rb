44187a4ed84d2d6da81a9feef83ab2fe9d00ad86
Benmuiruri <ben.muiruri13@gmail.com>
2023-08-10 11:35:29 +0300

    Chore: add Episode show view

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root "podcasts#index"
 
   resources :podcasts do
-    resources :episodes, only: [:index]
+    resources :episodes, only: [:index, :show]
     collection do
       post :fetch_episodes
     end
   end
+
+  get 'search', to: 'search#index'
 end
