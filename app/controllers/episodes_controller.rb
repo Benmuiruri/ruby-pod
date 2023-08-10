bef08d73f6f814463cc0062d86cad511b17ef244
Benmuiruri <ben.muiruri13@gmail.com>
2023-08-10 11:36:40 +0300

    Chore: add index show action

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,6 +3,10 @@
 class EpisodesController < ApplicationController
   def index
     @podcast = Podcast.find(params[:podcast_id])
-    @episodes = @podcast.episodes.order(release_date: :desc)
+    @episodes = @podcast.episodes.order(release_date: :desc).decorate
+  end
+
+  def show
+    @episode = Episode.find(params[:id]).decorate
   end
 end
